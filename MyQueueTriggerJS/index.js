79e3f8f33e8c7c01db5270cf5d93072d368e623f
Chris Brown <chris.brown@requitest.com>
2018-07-11 10:03:44 -0400

    trying this again

--- MyQueueTriggerJS/index.js
+++ MyQueueTriggerJS/index.js
@@ -1,4 +1,5 @@
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    context.bindings["my-output-queue"] = myQueueItem.toUpperCase()
     context.done();
 };
