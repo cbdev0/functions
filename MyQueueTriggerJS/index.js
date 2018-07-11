cf5aeb60c86e310f40a9bf4485a912097a60ee2e
Chris Brown <chris.brown@requitest.com>
2018-07-11 10:09:07 -0400

    fixing variable

--- MyQueueTriggerJS/index.js
+++ MyQueueTriggerJS/index.js
@@ -1,5 +1,5 @@
 module.exports = function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
-    context.bindings["my-output-queue"] = myQueueItem.toUpperCase()
+    context.bindings.myOutputQueueItem = myQueueItem.toUpperCase()
     context.done();
 };
